6b226f1624b097c18a6d32cc99cdadd6eb1224ed
cmoses8626 <airX@Chriss-MacBook-Air-3.local>
2014-09-06 12:40:14 -0400

    Better formatting, added order by hosp. death

--- patients_table.sql
+++ patients_table.sql
@@ -1,6 +1,9 @@
 --Export PATIENTS table for our Grand Mal patients
-SELECT DISTINCT ic.subject_id, ic.code, dp.hospital_expire_flg
+SELECT DISTINCT ic.subject_id, 
+	ic.code, 
+	dp.hospital_expire_flg
 FROM MIMIC2V26.icd9 ic
 	JOIN MIMIC2V26.d_patients dp
 ON ic.subject_id = dp.subject_id
 WHERE ic.code in ('345.2','345.3')
+ORDER BY dp.hospital_expire_flg DESC;
